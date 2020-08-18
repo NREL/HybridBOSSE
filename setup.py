9f8232c4acebd0f9732fd0b6347d143060fdc1e0
PB_Energy <parangat.bhaskar94@gmail.com>
2020-08-18 07:04:29 -0600

    Update v. to 0.9.4

--- setup.py
+++ setup.py
@@ -57,6 +57,6 @@ setup(
     # test_suite='tests',
     # tests_require=test_requirements,
     url='https://github.com/parangat94/hybridbosse',
-    version='0.9.3',
+    version='0.9.4',
     # zip_safe=False,
 )
