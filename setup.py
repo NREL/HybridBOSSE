91495f8eb0b8a5cb0937c16e2ad6bb22202bbe9e
PB_Energy <parangat.bhaskar94@gmail.com>
2020-08-18 05:45:25 -0600

    Update v. to 0.7

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/parangat94/hybridbosse',
-    version='0.6',
+    version='0.7',
     zip_safe=False,
 )
