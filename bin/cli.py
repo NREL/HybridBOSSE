c3950f74045f603c4843e950b63d358274dfb22a
Aaron Barker <barker59@gmail.com>
2020-10-01 12:29:49 -0600

    Adds openpyxl to requirements_dev.txt

--- bin/cli.py
+++ bin/cli.py
@@ -1,13 +1,13 @@
-"""Console script for hybrids_shared_infrastructure."""
+"""Console script for bin."""
 import sys
 import click
 
 
 @click.command()
 def main(args=None):
-    """Console script for hybrids_shared_infrastructure."""
+    """Console script for bin."""
     click.echo("Replace this message by putting your code into "
-               "hybrids_shared_infrastructure.cli.main")
+               "bin.cli.main")
     click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
 
